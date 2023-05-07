d57adbe6ff340abbe8193364dce000aae288cf87
caiyaya <21210860008@m.fudan.edu.cn>
2023-05-07 15:20:36 +0800

    router

--- coreweb/controller.go
+++ coreweb/controller.go
@@ -11,7 +11,6 @@ import (
 func FooControllerHandler(c *framework.Context) error {
 	finish := make(chan struct{}, 1)
 	panicChan := make(chan interface{}, 1)
-
 	durationCtx, cancel := context.WithTimeout(c.BaseContext(), time.Duration(1*time.Second))
 	defer cancel()
 
